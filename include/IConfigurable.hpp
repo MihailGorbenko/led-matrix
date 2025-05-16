850db4674c1b34dd79d3971b9a1649d0d5eb05bb
MihailGorbenko <gomihagle@gmail.com>
2025-05-16 12:18:49 +0300

    Update IConfigurable.hpp

--- include/IConfigurable.hpp
+++ include/IConfigurable.hpp
@@ -1,9 +1,23 @@
+#pragma once
+#include <ArduinoJson.h>
+
 class IConfigurable {
 public:
+    // Загрузка всех настроек из NVS
     virtual bool loadConfig() = 0;
+
+    // Сохранение всех текущих настроек в NVS
     virtual bool saveConfig() const = 0;
+
+    // Сброс всех настроек к значениям по умолчанию и сохранение в NVS
     virtual bool resetConfig() = 0;
-    virtual DynamicJsonDocument toJSON() const = 0;
+
+    // Сериализация всех настроек в JSON
+    virtual void toJSON(JsonObject& json) const = 0;
+
+    // Загрузка настроек из JSON
     virtual bool fromJSON(const JsonObject& json) = 0;
+
+    // Виртуальный деструктор
     virtual ~IConfigurable() = default;
 };
